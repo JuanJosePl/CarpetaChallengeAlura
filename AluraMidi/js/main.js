4fd0df5e70aa316b9d97d7b6267159f78dcd2c0f
JuanJosePl <poloj3614@gmail.com>
2024-05-20 22:03:33 -0500

    Agregando nuevo reto (AluraMidi)

---
+++ AluraMidi/js/main.js
@@ -0,0 +1,8 @@
+function playSonidoPom() {
+  document.querySelector("#sonido_tecla_pom").play();
+}
+
+/* document.querySelector('.tecla_pom').onclick = playSonidoPom; */
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
