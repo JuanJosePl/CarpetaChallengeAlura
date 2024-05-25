1193d4f63cb7b069488b16be0d2155a4badf4d80
JuanJosePl <poloj3614@gmail.com>
2024-05-25 12:00:49 -0500

    Agregando funcionalidad al AluraMidi y agregue nueva carpeta aluraFom como reto que hice

--- AluraMidi/js/main.js
+++ AluraMidi/js/main.js
@@ -4,17 +4,27 @@ function playSonido(idElementoAudio) {
 
 /* document.querySelector('.tecla_pom').onclick = playSonidoPom; */
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-
-for (let i = 0; i < listaDeTeclas.length; i++){
+for (let i = 0; i < listaDeTeclas.length; i++) {
   const tecla = listaDeTeclas[i];
   const instrumento = tecla.classList[1];
   console.log(instrumento);
 
-  const idAudio = `#sonido_${instrumento}`
+  const idAudio = `#sonido_${instrumento}`;
   console.log(idAudio);
-  tecla.onclick = function(){
-      playSonido(idAudio)
+
+  tecla.onclick = function () {
+    playSonido(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Enter" || evento.code === 'Space') {
+      tecla.classList.add("activa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("activa");
   };
 }
