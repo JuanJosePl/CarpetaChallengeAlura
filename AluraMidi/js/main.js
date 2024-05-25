8b8d1ac774d85400c7f0a104f1ae3e11a1f45b7e
JuanJosePl <poloj3614@gmail.com>
2024-05-24 22:44:17 -0500

    Agregando funcionalidades para el sonido

--- AluraMidi/js/main.js
+++ AluraMidi/js/main.js
@@ -1,8 +1,20 @@
-function playSonidoPom() {
-  document.querySelector("#sonido_tecla_pom").play();
+function playSonido(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 /* document.querySelector('.tecla_pom').onclick = playSonidoPom; */
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+
+for (let i = 0; i < listaDeTeclas.length; i++){
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
+  console.log(instrumento);
+
+  const idAudio = `#sonido_${instrumento}`
+  console.log(idAudio);
+  tecla.onclick = function(){
+      playSonido(idAudio)
+  };
+}
